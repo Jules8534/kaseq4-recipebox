00ca4b57a0d3cb935d192b530d3d00bc6e4c0aea
Benjamin Mark <benjmark@protonmail.com>
2020-04-29 15:38:37 -0400

    added initial models & test data to database

--- recipebox/models.py
+++ recipebox/models.py
@@ -1,3 +1,6 @@
+from django.db import models
+from django.utils import timezone
+
 """
 Author Model:
 
@@ -12,3 +15,21 @@ Recipe Model:
     Time Required (Charfield) (for example, "One hour")
     Instructions (TextField)
 """
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    byline = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class NewsItem(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField()
+    post_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} - {self.author}'
--- recipebox/urls.py
+++ recipebox/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from recipebox.models import Author, NewsItem
+
+admin.site.register(Author)
+admin.site.register(NewsItem)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
